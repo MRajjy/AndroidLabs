cf39741843c968db26587ec58d18485efae52caf
MRajjy <raja0060@algonquinlive.com>
2017-12-13 12:04:41 -0500

    another checkpoint for lab7

--- app/src/main/java/com/example/melissarajala/androidlabs/MessageDetails.java
+++ app/src/main/java/com/example/melissarajala/androidlabs/MessageDetails.java
@@ -17,41 +17,15 @@ public class MessageDetails extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_details);
 
+        Bundle info = getIntent().getExtras();
 
-
-//        Bundle info = getIntent().getExtras();
-
-        //info.putString("Key", "From phone");
         //start Transaction to insert fragment in screen:
-//        FragmentTransaction ft =  getFragmentManager().beginTransaction();
-//        final MessageFragment mf = new MessageFragment();
-//        mf.setArguments(info);
-//        ft.add(R.id.frame_layout, mf );
-//        ft.commit();
-
-
-
-        removeButton = (Button)findViewById(R.id.deleteBtn);
-        removeButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                if(layout = findViewById(R.id.frame_layout) != null)
-                    {
-                        //start Transaction to insert fragment in screen:
-//                        FragmentTransaction ft =  getFragmentManager().beginTransaction();
-//                        ft.remove(mf);
-//                        ft.commit();
-
-                    }
-                    else
-                    {
-
-                    }
-                }
-            });
-
-
+        FragmentTransaction ft =  getFragmentManager().beginTransaction();
+        MessageFragment mf = new MessageFragment();
+        mf.setArguments(info);
+        ft.add(R.id.frame_layout, mf );
+        ft.commit();
 
     }
-
 
 }
